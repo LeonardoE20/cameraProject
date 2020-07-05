eaad1b63ae4bbd59eb1b1e9d03d9d775b5971f0c
Leonardo Escudero <leonardolearns@gmail.com>
2020-07-05 13:42:38 -0700

    Added the take picture and the project now works

--- cameraApp/platforms/browser/www/js/index.js
+++ cameraApp/platforms/browser/www/js/index.js
@@ -17,30 +17,90 @@
  * under the License.
  */
 var app = {
-    // Application Constructor
-    initialize: function() {
-        document.addEventListener('deviceready', this.onDeviceReady.bind(this), false);
-    },
-
-    // deviceready Event Handler
-    //
-    // Bind any cordova events here. Common events are:
-    // 'pause', 'resume', etc.
-    onDeviceReady: function() {
-        this.receivedEvent('deviceready');
-    },
-
-    // Update DOM on a Received Event
-    receivedEvent: function(id) {
-        var parentElement = document.getElementById(id);
-        var listeningElement = parentElement.querySelector('.listening');
-        var receivedElement = parentElement.querySelector('.received');
-
-        listeningElement.setAttribute('style', 'display:none;');
-        receivedElement.setAttribute('style', 'display:block;');
-
-        console.log('Received Event: ' + id);
-    }
+	// Application Constructor
+	initialize: function() {
+		document.getElementById("btn").addEventListener("click", app.takePhoto);
+	},
+	// deviceready Event Handler
+	//
+	// Bind any cordova events here. Common events are:
+	// 'pause', 'resume', etc.
+	onDeviceReady: function() {
+		this.receivedEvent("deviceready");
+	},
+	// take a picture!
+	takePhoto: function() {
+		let opts = {
+			quality: 80,
+			destinationType: Camera.DestinationType.FILE_URI,
+			sourceType: Camera.PictureSourceType.CAMERA,
+			encodingType: Camera.EncodingType.JPEG,
+			cameraDirection: Camera.Direction.BACK,
+			targetWidth: 300,
+			targetHeight: 400
+		};
+		console.log("Button Clicked");
+		navigator.camera.getPicture(app.onSuccess, app.onFail, opts);
+	},
+	onSuccess: function(imageURI) {
+		// document.getElementById('msg').textContent = imageURI;
+		document.getElementById("photo").src = "data:image/jpeg;base64," + imageURI;
+	},
+	onFail: function(message) {
+		alert("Failed because: " + message);
+	}
 };
-
 app.initialize();
+
+// /*
+//  * Licensed to the Apache Software Foundation (ASF) under one
+//  * or more contributor license agreements.  See the NOTICE file
+//  * distributed with this work for additional information
+//  * regarding copyright ownership.  The ASF licenses this file
+//  * to you under the Apache License, Version 2.0 (the
+//  * "License"); you may not use this file except in compliance
+//  * with the License.  You may obtain a copy of the License at
+//  *
+//  * http://www.apache.org/licenses/LICENSE-2.0
+//  *
+//  * Unless required by applicable law or agreed to in writing,
+//  * software distributed under the License is distributed on an
+//  * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+//  * KIND, either express or implied.  See the License for the
+//  * specific language governing permissions and limitations
+//  * under the License.
+//  */
+// var app = {
+// 	// Application Constructor
+// 	initialize: function() {
+// 		document.getElementById("btn").addEventListener("click", app.takePhoto);
+// 	},
+
+// 	// deviceready Event Handler
+// 	//
+// 	// Bind any cordova events here. Common events are:
+// 	// 'pause', 'resume', etc.
+// 	onDeviceReady: function() {
+// 		this.receivedEvent("deviceready");
+// 	},
+
+// 	// take a picture!
+// 	takePhoto: function() {
+// 		console.log("Button Clicked");
+// 		navigator.camera.getPicture(app.onSuccess, app.onFail, {
+// 			quality: 50,
+// 			destinationType: Camera.DestinationType.Data_URL //FILE_URI
+// 		});
+// 	},
+
+// 	onSuccess: function(imageURI) {
+// 		var image = document.getElementById("photo");
+// 		image.src = imageURI;
+// 	},
+
+// 	onFail: function(message) {
+// 		alert("Failed because: " + message);
+// 	}
+// };
+
+// app.initialize();
--- cameraApp/www/js/index.js
+++ cameraApp/www/js/index.js
@@ -21,7 +21,6 @@ var app = {
 	initialize: function() {
 		document.getElementById("btn").addEventListener("click", app.takePhoto);
 	},
-
 	// deviceready Event Handler
 	//
 	// Bind any cordova events here. Common events are:
@@ -29,24 +28,79 @@ var app = {
 	onDeviceReady: function() {
 		this.receivedEvent("deviceready");
 	},
-
 	// take a picture!
 	takePhoto: function() {
+		let opts = {
+			quality: 80,
+			destinationType: Camera.DestinationType.FILE_URI,
+			sourceType: Camera.PictureSourceType.CAMERA,
+			encodingType: Camera.EncodingType.JPEG,
+			cameraDirection: Camera.Direction.BACK,
+			targetWidth: 300,
+			targetHeight: 400
+		};
 		console.log("Button Clicked");
-		navigator.camera.getPicture(onSuccess, onFail, {
-			quality: 50,
-			destinationType: Camera.DestinationType.FILE_URI
-		});
+		navigator.camera.getPicture(app.onSuccess, app.onFail, opts);
 	},
-
 	onSuccess: function(imageURI) {
-		var image = document.getElementById("myImage");
-		image.src = imageURI;
+		// document.getElementById('msg').textContent = imageURI;
+		document.getElementById("photo").src = "data:image/jpeg;base64," + imageURI;
 	},
-
 	onFail: function(message) {
 		alert("Failed because: " + message);
 	}
 };
-
 app.initialize();
+
+// /*
+//  * Licensed to the Apache Software Foundation (ASF) under one
+//  * or more contributor license agreements.  See the NOTICE file
+//  * distributed with this work for additional information
+//  * regarding copyright ownership.  The ASF licenses this file
+//  * to you under the Apache License, Version 2.0 (the
+//  * "License"); you may not use this file except in compliance
+//  * with the License.  You may obtain a copy of the License at
+//  *
+//  * http://www.apache.org/licenses/LICENSE-2.0
+//  *
+//  * Unless required by applicable law or agreed to in writing,
+//  * software distributed under the License is distributed on an
+//  * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+//  * KIND, either express or implied.  See the License for the
+//  * specific language governing permissions and limitations
+//  * under the License.
+//  */
+// var app = {
+// 	// Application Constructor
+// 	initialize: function() {
+// 		document.getElementById("btn").addEventListener("click", app.takePhoto);
+// 	},
+
+// 	// deviceready Event Handler
+// 	//
+// 	// Bind any cordova events here. Common events are:
+// 	// 'pause', 'resume', etc.
+// 	onDeviceReady: function() {
+// 		this.receivedEvent("deviceready");
+// 	},
+
+// 	// take a picture!
+// 	takePhoto: function() {
+// 		console.log("Button Clicked");
+// 		navigator.camera.getPicture(app.onSuccess, app.onFail, {
+// 			quality: 50,
+// 			destinationType: Camera.DestinationType.Data_URL //FILE_URI
+// 		});
+// 	},
+
+// 	onSuccess: function(imageURI) {
+// 		var image = document.getElementById("photo");
+// 		image.src = imageURI;
+// 	},
+
+// 	onFail: function(message) {
+// 		alert("Failed because: " + message);
+// 	}
+// };
+
+// app.initialize();
