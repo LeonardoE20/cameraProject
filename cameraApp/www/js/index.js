0373cae8feed378e6ed90ef8d07a1278e7ee4566
Leonardo Escudero <leonardolearns@gmail.com>
2020-07-05 12:59:36 -0700

    Debugged some code again in js

--- cameraApp/www/js/index.js
+++ cameraApp/www/js/index.js
@@ -33,6 +33,19 @@ var app = {
 	// take a picture!
 	takePhoto: function() {
 		console.log("Button Clicked");
+		navigator.camera.getPicture(onSuccess, onFail, {
+			quality: 50,
+			destinationType: Camera.DestinationType.FILE_URI
+		});
+	},
+
+	onSuccess: function(imageURI) {
+		var image = document.getElementById("myImage");
+		image.src = imageURI;
+	},
+
+	onFail: function(message) {
+		alert("Failed because: " + message);
 	}
 };
 
